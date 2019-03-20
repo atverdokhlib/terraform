6d7e650b11da67a69aaaace3fb15777641e6f4a8
Andriy Tverdokhlib <a_tv@ukr.net>
2019-03-20 21:01:52 +0200

    Prov

--- provider.tf
+++ provider.tf
@@ -3,5 +3,5 @@ provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "=1.22.0"
   subscription_id = "d587856f-5eca-4199-b173-37634f44867d"
-  tenant_id       = "a4df60bd-8703-453a-9300-8fff26941972"  
+  tenant_id       = "XXXXXXX"  
 }
