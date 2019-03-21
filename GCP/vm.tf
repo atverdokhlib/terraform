5db1ed103be590aebea133bafa494783964835c2
Andriy Tverdokhlib <a_tv@ukr.net>
2019-03-21 14:30:18 +0200

    ssh

--- GCP/vm.tf
+++ GCP/vm.tf
@@ -1,8 +1,7 @@
 	
 
 resource "google_compute_instance" "test" {
-  count        = 1 // Adjust as desired
-  name         = "test${count.index + 1}" // yields "test1", "test2", etc. It's also the machine's name and hostname
+  name         = "${var.prefix}-vm" // yields "test1", "test2", etc. It's also the machine's name and hostname
   machine_type = "f1-micro" // smallest (CPU &amp; RAM) available instance
   zone         = "${var.zone}" // yields "europe-west1-d" as setup previously. Places your VM in Europe
  
@@ -19,5 +18,16 @@ resource "google_compute_instance" "test" {
       // Ephemeral IP - leaving this block empty will generate a new external IP and assign it to the machine
     }
   }
+  metadata {
+   sshKeys = "andy:${file(".ssh/id_rsa.pub")}"
+ }
+metadata_startup_script = <<SCRIPT
+sudo apt update
+sudo apt install apache2
+sudo chmod 777 /var/www/html/index.html
+sudo > /var/www/html/index.html
+sudo curl ifconfig.co > /var/www/html/index.html
+sudo ervice apache restart
+SCRIPT
 }
 
